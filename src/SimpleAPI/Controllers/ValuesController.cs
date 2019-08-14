efaafbcb92e9c77eee16ab4fb1ebfccdc070eab1
gazmack86 <39755491+gazmack86@users.noreply.github.com>
2019-08-15 09:45:42 +1000

    Updates to controller file

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "Gareth MacKinnon";
+            return "Gaz MacKinnon";
         }
 
         // POST api/values
